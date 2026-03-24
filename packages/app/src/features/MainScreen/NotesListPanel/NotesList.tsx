650924e1d6ae4caa5ec13b5c82c404e4ee3936dd
Irina Katsyuta <katsyuta@proton.me>
2026-03-24 22:07:40 +0100

    refactor: update api

--- packages/app/src/features/MainScreen/NotesListPanel/NotesList.tsx
+++ packages/app/src/features/MainScreen/NotesListPanel/NotesList.tsx
@@ -199,12 +199,7 @@ export const NotesList: FC<NotesListProps> = () => {
 										);
 									}}
 									onDoubleClick={() => {
-										dispatch(
-											workspacesApi.setTemporaryNote({
-												...workspaceData,
-												noteId: null,
-											}),
-										);
+										noteActions.click(note.id, { temporary: false });
 									}}
 								/>
 							);
