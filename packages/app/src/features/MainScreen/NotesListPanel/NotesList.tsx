6a1ff8f59f4aa8eb588650f764c00ff084c9c3fe
Irina Katsyuta <katsyuta@proton.me>
2026-03-24 21:27:49 +0100

    feat: add temporary note

--- packages/app/src/features/MainScreen/NotesListPanel/NotesList.tsx
+++ packages/app/src/features/MainScreen/NotesListPanel/NotesList.tsx
@@ -198,6 +198,14 @@ export const NotesList: FC<NotesListProps> = () => {
 											},
 										);
 									}}
+									onDoubleClick={() => {
+										dispatch(
+											workspacesApi.setTemporaryNote({
+												...workspaceData,
+												noteId: null,
+											}),
+										);
+									}}
 								/>
 							);
 						})}
