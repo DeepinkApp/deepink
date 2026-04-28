b7db9fa5e2befd2173ed6eb0410d6a33aa6c9da6
Irina Katsyuta <katsyuta@proton.me>
2026-04-28 22:57:03 +0200

    chore: rename

--- packages/app/src/core/database/sqlite/migrations/index.ts
+++ packages/app/src/core/database/sqlite/migrations/index.ts
@@ -34,7 +34,7 @@ export const getMigrationsList = async (): Promise<SQLiteMigration[]> =>
 	Promise.all([
 		convertSQLToMigrationObject('1_init_db', import('./sql/1_init_db.sql')),
 		convertSQLToMigrationObject(
-			'2_add_notes_pinned',
-			import('./sql/2_add_notes_pinned.sql'),
+			'2_add_pinned_to_notes',
+			import('./sql/2_add_pinned_to_notes.sql'),
 		),
 	]);
