9bb4a229697a6a627b6b902af2bb74e7aa08d613
Irina Katsyuta <katsyuta@proton.me>
2026-05-19 15:57:18 +0200

    refactor: remove higliting note after pin

--- packages/app/src/components/NotePreview/NotePreview.theme.ts
+++ packages/app/src/components/NotePreview/NotePreview.theme.ts
@@ -60,12 +60,6 @@ export const notePreviewRecipe = defineSlotRecipe({
 						color: 'control.option.active.foreground',
 					},
 				},
-
-				'&[data-flashing]': {
-					boxShadow:
-						'inset 4px 0 0 0 var(--chakra-colors-control-action-active-background)',
-				},
-				transition: 'box-shadow 0.2s ease',
 			},
 		},
 	},
