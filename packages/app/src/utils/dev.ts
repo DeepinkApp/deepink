26c7b727f5a551de324a6a9b8af2e487dd65d52d
Robert Vitonsky <robert@vitonsky.net>
2026-06-05 14:50:24 +0200

    chore: add session flags

--- packages/app/src/utils/dev.ts
+++ packages/app/src/utils/dev.ts
@@ -1,1 +1,2 @@
-export const getDevFlag = (name: string) => (window as any)[Symbol.for(name)];
+export const getDevFlag = (name: string) =>
+	(window as any)[Symbol.for(name)] || window.sessionStorage[name];
