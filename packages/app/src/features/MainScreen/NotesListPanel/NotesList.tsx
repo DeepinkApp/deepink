210c33e86cd86405b3fb2ef09929ff54f98b8831
Irina Katsyuta <katsyuta@proton.me>
2026-06-15 21:50:27 +0200

    chore: disable eslint rule

--- packages/app/src/features/MainScreen/NotesListPanel/NotesList.tsx
+++ packages/app/src/features/MainScreen/NotesListPanel/NotesList.tsx
@@ -49,6 +49,7 @@ export const NotesList: FC<NotesListProps> = () => {
 	const parentRef = useRef<HTMLDivElement>(null);
 	const isActiveWorkspace = useIsActiveWorkspace();
 
+	// eslint-disable-next-line react-hooks/incompatible-library
 	const virtualizer = useVirtualizer({
 		enabled: isActiveWorkspace,
 		count: noteIds.length,
